696907f56c05e88c4be20bfce41c8a2b81bb28d1
Lia-Duggan <92882948+Lia-Duggan@users.noreply.github.com>
2021-10-22 14:18:01 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Sad)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Surprised)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.Angry)
+})
